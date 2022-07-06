7414fa1b48880fe5b0ab1ed02566dcce76977d65
abhi16180 <4ni19cs003_a@nie.ac.in>
2022-07-06 20:14:06 +0530

    update

---
+++ Java/LAB1/AddTwoNumbers.java
@@ -0,0 +1,17 @@
+//program to add two numbers using scanner
+import java.util.Scanner;
+class AddTwoNumbers {
+   
+    public static void main(String args[]){
+        Scanner s= new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a= s.nextInt();
+        System.out.println("Enter the second number");
+        int b=s.nextInt();
+        int sum=a+b;
+        System.out.println(sum);
+        s.close();
+ 
+    }
+ 
+}
