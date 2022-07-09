ba0f69d3c6a3388027644a990b37d3d76452c03e
abhi16180 <4ni19cs003_a@nie.ac.in>
2022-07-09 09:54:39 +0530

    update

---
+++ Lab/PART A/Overloading.java
@@ -0,0 +1,28 @@
+
+
+public class Overloading {
+    Overloading() {
+        System.out.println("Hey there");
+    }
+
+    Overloading(String s) {
+        System.out.println("Hello ," + s);
+    }
+
+    void display(String s) {
+        System.out.println(s);
+    }
+
+    void display(String s, String p) {
+        System.out.println(s + p);
+    }
+
+    public static void main(String args[]) {
+        // constructor overloading
+        Overloading obj1 = new Overloading();
+        Overloading obj2 = new Overloading("Matrix");
+        obj1.display("Display funtion with one argumennt");
+        obj1.display("Display funtion", "with two arguments");
+    }
+
+}
